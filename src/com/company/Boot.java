865ecb54d66de2472cc98071541caab5beb76621
chrop01 <91630967+chrop01@users.noreply.github.com>
2022-01-30 16:00:53 +0100

    added bootloop

--- src/com/company/Boot.java
+++ src/com/company/Boot.java
@@ -6,18 +6,26 @@ import org.lwjgl.opengl.DisplayMode;
 
 public class Boot {
 
-    public Boot()
-    {
+    public Boot() {
         Display.setTitle("2d");
         try {
-            Display.setDisplayMode(new DisplayMode(600,400));
+            Display.setDisplayMode(new DisplayMode(600, 400));
             Display.create(); // Screen
         } catch (LWJGLException e) {
             e.printStackTrace();
         }
+
+
+
+        while (!Display.isCloseRequested()) {
+            Display.update();
+            Display.sync(60);
+        }
+
+        Display.destroy();
     }
 
     public static void main(String[] args) {
-	// write your code here
+        new Boot();
     }
 }
